41243c77bf3fcbefae01dbe1b1f2b057adca1dab
vitor-matheus <vitormatheusob@gmail.com>
2023-08-17 02:10:05 -0300

    Calculadora

---
+++ Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace Calculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            textBox1 = new TextBox();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            Resultado = new Button();
+            AC = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(8, 104);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(89, 104);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 1;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(170, 104);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 2;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(8, 133);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 3;
+            button4.Text = "4";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(89, 133);
+            button5.Name = "button5";
+            button5.Size = new Size(75, 23);
+            button5.TabIndex = 4;
+            button5.Text = "5";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(170, 133);
+            button6.Name = "button6";
+            button6.Size = new Size(75, 23);
+            button6.TabIndex = 5;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(8, 162);
+            button7.Name = "button7";
+            button7.Size = new Size(75, 23);
+            button7.TabIndex = 6;
+            button7.Text = "7";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(89, 162);
+            button8.Name = "button8";
+            button8.Size = new Size(75, 23);
+            button8.TabIndex = 7;
+            button8.Text = "8";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(170, 162);
+            button9.Name = "button9";
+            button9.Size = new Size(75, 23);
+            button9.TabIndex = 8;
+            button9.Text = "9";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(89, 191);
+            button10.Name = "button10";
+            button10.Size = new Size(75, 23);
+            button10.TabIndex = 9;
+            button10.Text = "0";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button0_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.Control;
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Enabled = false;
+            textBox1.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox1.ForeColor = Color.Black;
+            textBox1.Location = new Point(13, 21);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(324, 48);
+            textBox1.TabIndex = 10;
+            textBox1.TabStop = false;
+            textBox1.Text = "0";
+            textBox1.TextAlign = HorizontalAlignment.Right;
+            textBox1.BackColorChanged += textBox1_TextChanged;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(263, 108);
+            button11.Name = "button11";
+            button11.Size = new Size(75, 23);
+            button11.TabIndex = 11;
+            button11.Text = "+";
+            button11.UseVisualStyleBackColor = true;
+            button11.Click += button11_Click;
+            // 
+            // button12
+            // 
+            button12.Location = new Point(263, 137);
+            button12.Name = "button12";
+            button12.Size = new Size(75, 23);
+            button12.TabIndex = 12;
+            button12.Text = "-";
+            button12.UseVisualStyleBackColor = true;
+            button12.Click += button12_Click;
+            // 
+            // button13
+            // 
+            button13.Location = new Point(263, 166);
+            button13.Name = "button13";
+            button13.Size = new Size(75, 23);
+            button13.TabIndex = 13;
+            button13.Text = "/";
+            button13.UseVisualStyleBackColor = true;
+            button13.Click += button13_Click;
+            // 
+            // button14
+            // 
+            button14.Location = new Point(263, 195);
+            button14.Name = "button14";
+            button14.Size = new Size(75, 23);
+            button14.TabIndex = 14;
+            button14.Text = "*";
+            button14.UseVisualStyleBackColor = true;
+            button14.Click += button14_Click;
+            // 
+            // Resultado
+            // 
+            Resultado.Location = new Point(263, 224);
+            Resultado.Name = "Resultado";
+            Resultado.Size = new Size(75, 23);
+            Resultado.TabIndex = 15;
+            Resultado.Text = "=";
+            Resultado.UseVisualStyleBackColor = true;
+            Resultado.Click += Resultado_Click;
+            // 
+            // AC
+            // 
+            AC.Location = new Point(263, 75);
+            AC.Name = "AC";
+            AC.Size = new Size(74, 27);
+            AC.TabIndex = 16;
+            AC.Text = "AC";
+            AC.UseVisualStyleBackColor = true;
+            AC.Click += button15_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(351, 254);
+            Controls.Add(AC);
+            Controls.Add(Resultado);
+            Controls.Add(button14);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(textBox1);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private TextBox textBox1;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button Resultado;
+        private Button AC;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,359 @@
+using System.Configuration;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        //Propriedade
+        public string Operador { get; set; } // Propriedade que definine o operador que será utilizado
+        public bool multEscolhido { get; set; }// Propriedade que definine o a exceção de multiplicar por zero .
+        public char[] charArray // Propriedade que converte a string presente na textBox em um array de caracteres para que possa ser trabalhado a exclusão do zero a esquerda. 
+        {
+            get
+            {
+                this.arrayCriado = textBox1.Text.ToCharArray(0, textBox1.Text.Length);           
+                return this.arrayCriado;
+            }
+        }
+        //Atributos
+        public char[] arrayCriado; // Atributo que recebe o array de caracteres que será trabalhado nas exceções
+        public string[] valor = new string[2]; // Atributo responsável por receber um Array de duas posições que controla os valores das variáveis que estarão no cálculo realizado pela cálculadora.
+        public int posicaoArray = 0; // Atributo que contém a posição em que o array de valor está, seu valor padrão inícial é zero.
+        public bool divisaoPorZeroConfirmacao;//Atributo que retorna verdadeiro caso sejá realizado uma divisão por zero, reiniciando a calculadora para estado inicíal.
+        public int quantidadeVezesBotaoClicado;//Atributo responsável por atuar como contador de vezes que o botão que insere as operações matemáticas é clicado.
+        //Métodos
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void excluirZeroAEsquerda()
+        {
+            if (this.charArray[0] == '0')
+            {
+                textBox1.Clear();
+            };
+        }
+        public void exibirExcecaoDividirPorZero(bool confirmacao)
+        {
+            if (confirmacao == true)
+            {
+                textBox1.Clear();
+                textBox1.BackColor = SystemColors.Control;
+                textBox1.Font = new Font(textBox1.Font.FontFamily, 24);
+                textBox1.Font = new Font(textBox1.Font, FontStyle.Bold);
+                this.valor[0] = "0";
+                this.valor[1] = "0";
+                divisaoPorZeroConfirmacao = false;
+                posicaoArray = 0;
+            }
+        }
+        public int clicouOperador(bool confirmarOperador)
+        {
+            if (confirmarOperador == true)
+            { posicaoArray = 1; }
+       
+            return posicaoArray;
+        }
+        public void realizarConta(string operador)
+        {
+            long resultado;
+            float resultadoFloat;
+
+            switch (operador)
+            {
+                case "+":
+                    resultado = Int64.Parse(this.valor[0]) + Int64.Parse(this.valor[1]);
+                    this.valor[0] = resultado.ToString();
+                    this.valor[1] = "0";
+                    textBox1.Text = this.valor[0];
+                    break;
+
+                case "-":
+                    resultado = Int64.Parse(this.valor[0]) - Int64.Parse(this.valor[1]);
+                    this.valor[0] = resultado.ToString();
+                    this.valor[1] = "0";
+                    textBox1.Text = this.valor[0];
+                    break;
+
+                case "*":
+                    resultado = Int64.Parse(this.valor[0]) * Int64.Parse(this.valor[1]);
+                    this.valor[0] = resultado.ToString();
+                    this.valor[1] = "0";
+                    textBox1.Text = this.valor[0];
+                    break;
+                case "/":
+                    try
+                    {
+                        resultadoFloat = float.Parse(this.valor[0]) / float.Parse(this.valor[1]);
+                        this.valor[0] = resultadoFloat.ToString();
+                        this.valor[1] = "0";
+                        textBox1.Text = this.valor[0];
+                        break;
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        divisaoPorZeroConfirmacao = true;
+                        textBox1.Font = new Font(textBox1.Font.FontFamily, 15);
+                        textBox1.Text = "Não é possível dividir por zero";
+                        break;
+                    }
+                default:
+                    this.valor[0] = "0";
+                    this.valor[1] = "0";
+                    break;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            excluirZeroAEsquerda();
+
+            if(quantidadeVezesBotaoClicado > 1 )
+            {
+                textBox1.Clear();
+                textBox1.Text += this.valor[1];
+                excluirZeroAEsquerda();
+            }
+            exibirExcecaoDividirPorZero(divisaoPorZeroConfirmacao);
+            this.valor[posicaoArray] = textBox1.Text += "1";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            excluirZeroAEsquerda();
+
+            if (quantidadeVezesBotaoClicado > 1)
+            {
+                textBox1.Clear();
+                textBox1.Text += this.valor[1];
+                excluirZeroAEsquerda();
+            }
+            exibirExcecaoDividirPorZero(divisaoPorZeroConfirmacao);
+            int valor = 2;
+            this.valor[posicaoArray] = textBox1.Text += Convert.ToString(valor);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            excluirZeroAEsquerda();
+
+            if (quantidadeVezesBotaoClicado > 1)
+            {
+                textBox1.Clear();
+                textBox1.Text += this.valor[1];
+                excluirZeroAEsquerda();
+            }
+            exibirExcecaoDividirPorZero(divisaoPorZeroConfirmacao);
+            int valor = 3;
+            this.valor[posicaoArray] = textBox1.Text += Convert.ToString(valor);
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            excluirZeroAEsquerda();
+
+            if (quantidadeVezesBotaoClicado > 1)
+            {
+                textBox1.Clear();
+                textBox1.Text += this.valor[1];
+                excluirZeroAEsquerda();
+            }
+            exibirExcecaoDividirPorZero(divisaoPorZeroConfirmacao);
+            int valor = 4;
+            this.valor[posicaoArray] = textBox1.Text += Convert.ToString(valor);
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            excluirZeroAEsquerda();
+
+            if (quantidadeVezesBotaoClicado > 1)
+            {
+                textBox1.Clear();
+                textBox1.Text += this.valor[1];
+                excluirZeroAEsquerda();
+            }
+            exibirExcecaoDividirPorZero(divisaoPorZeroConfirmacao);
+            int valor = 5;
+            this.valor[posicaoArray] = textBox1.Text += Convert.ToString(valor);
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            excluirZeroAEsquerda();
+
+            if (quantidadeVezesBotaoClicado > 1)
+            {
+                textBox1.Clear();
+                textBox1.Text += this.valor[1];
+                excluirZeroAEsquerda();
+            }
+            exibirExcecaoDividirPorZero(divisaoPorZeroConfirmacao);
+            int valor = 6;
+            this.valor[posicaoArray] = textBox1.Text += Convert.ToString(valor);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            excluirZeroAEsquerda();
+
+            if (quantidadeVezesBotaoClicado > 1)
+            {
+                textBox1.Clear();
+                textBox1.Text += this.valor[1];
+                excluirZeroAEsquerda();
+            }
+            exibirExcecaoDividirPorZero(divisaoPorZeroConfirmacao);
+            int valor = 7;
+            this.valor[posicaoArray] = textBox1.Text += Convert.ToString(valor);
+        }
+        private void button8_Click(object sender, EventArgs e)
+        {
+            excluirZeroAEsquerda();
+            if (quantidadeVezesBotaoClicado > 1)
+            {
+                textBox1.Clear();
+                textBox1.Text += this.valor[1];
+                excluirZeroAEsquerda();
+            }
+            exibirExcecaoDividirPorZero(divisaoPorZeroConfirmacao);
+            int valor = 8;
+            this.valor[posicaoArray] = textBox1.Text += Convert.ToString(valor);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            excluirZeroAEsquerda();
+
+            if (quantidadeVezesBotaoClicado > 1)
+            {
+                textBox1.Clear();
+                textBox1.Text += this.valor[1];
+                excluirZeroAEsquerda();
+            }
+            exibirExcecaoDividirPorZero(divisaoPorZeroConfirmacao);
+            int valor = 9;
+            this.valor[posicaoArray] = textBox1.Text += Convert.ToString(valor);
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            if (this.charArray[0] == '0')
+            {
+                return;
+            }
+            
+            int valor = 0;
+            this.valor[posicaoArray] = textBox1.Text += Convert.ToString(valor);
+        }
+
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            string soma = "+";
+            quantidadeVezesBotaoClicado++;
+
+            if(quantidadeVezesBotaoClicado > 1)
+            {
+                realizarConta(soma);
+                textBox1.Text = this.valor[0];         
+            }
+            else
+            {
+                textBox1.Clear();
+                textBox1.Text = "0";
+                this.Operador = soma;
+                clicouOperador(true);
+            }
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+
+            string subtracao = "-";
+            quantidadeVezesBotaoClicado++;
+
+            if (quantidadeVezesBotaoClicado > 1) 
+            {
+                realizarConta(subtracao);
+                textBox1.Text = this.valor[0];
+            }
+            else
+            {
+                textBox1.Clear();
+                textBox1.Text = "0";
+                this.Operador = subtracao;
+                clicouOperador(true);
+            }
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            string divisao = "/";
+            quantidadeVezesBotaoClicado++;
+
+            if (quantidadeVezesBotaoClicado > 1)
+            {
+                realizarConta(divisao);
+                textBox1.Text = this.valor[0];
+            }
+            else
+            {
+
+                textBox1.Clear();
+                textBox1.Text = "0";
+                this.Operador = divisao;
+                clicouOperador(true);
+            }
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            string multiplicacao = "*";
+            quantidadeVezesBotaoClicado++;
+
+            if (quantidadeVezesBotaoClicado > 1)
+            {
+                realizarConta(multiplicacao);
+                textBox1.Text = this.valor[0];
+            }
+            else
+            {
+                textBox1.Clear();
+                textBox1.Text = "0";
+                this.Operador = multiplicacao;
+                clicouOperador(true);
+
+            }
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox1.Text = this.valor[0] = "0";
+            this.valor[1] = "0";
+            clicouOperador(false);
+            posicaoArray = 0;
+            quantidadeVezesBotaoClicado = 0;
+            divisaoPorZeroConfirmacao = false;
+
+        }
+
+
+        private void Resultado_Click(object sender, EventArgs e)
+        {
+            this.posicaoArray = 0;
+
+            string operador = this.Operador;
+
+            quantidadeVezesBotaoClicado = 0;
+
+            realizarConta(operador);
+        }
+    }
+}
